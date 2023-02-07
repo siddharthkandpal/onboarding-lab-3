b547366f76b96076ff80e0efd7cfe60486d7e483
Siddharth Kandpal <123426199+siddharthkandpal@users.noreply.github.com>
2023-02-07 17:57:34 -0500

    Update Exercise2.sv

--- rtl/Exercise2.sv
+++ rtl/Exercise2.sv
@@ -16,3 +16,23 @@ module Exercise2 (
     if (reset) out <= ~init;
     else out <= {out[14:0], out[10] ^ out[8] ^ out[3] ^ out[1]};
 endmodule
+
+module testbench;
+  reg clk, reset;
+  reg [15:0] init;
+  logic [15:0] out;
+  
+  Exercise2 UUT ( .clk(clk), .reset(reset), .init(init), .out(out));
+  
+  initial 
+    begin
+      clk = 0;
+      rst = 1;
+       #10
+      rst = 0;
+    end
+  always @ (negedge clk) begin
+    #10 
+    clk <= ~clk;
+  end
+endmodule
