aef35dfbdf22edfa8ead3a2f84ed1a1b5ef0c030
Siddharth Kandpal <123426199+siddharthkandpal@users.noreply.github.com>
2023-02-10 22:10:28 -0500

    Updated ex 4

--- rtl/Exercise4.sv
+++ rtl/Exercise4.sv
@@ -31,3 +31,55 @@ module Exercise4 (
       endcase
   end
 endmodule
+
+//tb
+//incldue namespace std
+..
+
+
+int orand(uint8_t alpha, uint8_t beta, uint8_t gamma){
+  uint16_t result;
+  result = alpha | (beta & gamma);
+  retuen result;
+}
+  
+int main(){
+uint16_t expectedvalue;
+  
+  Exercise3 model;
+  for (cs = 0;cs < 2; cs = cs + 1){
+    for(sel = 0; sel < 4; sel = sel + 1){
+      model.alpha = 10;
+      model.beta = 20;
+      model.gamma = 30;
+      model.eval();
+    }
+  }
+  
+  while(expected value != 0){
+    if(sel = 0 && cs = 1){
+      expected value = 10;
+    }
+    else if(sel = 1 && cs = 1){
+      expected value = 20;
+    }
+    else if(sel = 2 && cs = 1){
+      expected value = 30;
+    }
+    else if(sel > 2 && cs = 1){
+      expected value = orand( 10, 20, 30 );
+    }
+    else{
+      if(cs = 0){
+        expected value = 0;
+      }
+  }
+    
+    if( model.out != expectedvalue ){
+      return 1;
+    }
+    std::cout<<"expected value is : "<< expectedvalue <<std::endl;
+  }
+  
+                             
+  
