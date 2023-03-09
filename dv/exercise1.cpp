c0dc34d7fd61bc903d1b7e9c79b7e7edc9d30a1a
Siddharth Kandpal <123426199+siddharthkandpal@users.noreply.github.com>
2023-03-09 17:14:29 -0500

    Update exercise1.cpp

--- dv/exercise1.cpp
+++ dv/exercise1.cpp
@@ -1,5 +1,17 @@
 #include <VExercise1.h>
-
+     
 int main() {
+  int h,i,k,a,b,op;
   VExercise1 model;
+  for(h = 0; h < 3; h = h + 1){
+  for (i = 0; i < 256; i = i + 1){
+    for (k = 0; k < 256; k = k + 1){
+      
+    model.op = h;
+    model.a = i;
+    model.b = k;
+    model.eval();
+  }
+ }
+  }
 }
