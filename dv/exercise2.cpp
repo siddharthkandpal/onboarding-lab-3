9508ecdb9e941cbafe020f27490f652e5663700d
Siddharth Kandpal <123426199+siddharthkandpal@users.noreply.github.com>
2023-03-09 17:14:56 -0500

    Update exercise2.cpp

--- dv/exercise2.cpp
+++ dv/exercise2.cpp
@@ -1,5 +1,24 @@
 #include <VExercise2.h>
 
 int main() {
+  uint8_t expectedvalue;
+  int reset, init, clk, out;
   VExercise2 model;
+     for(reset = 0; reset < 2; reset++){
+    model.clk = 0;
+    model.init = 15;
+    model.eval();
+    }
+  
+  if(reset = 1){
+    expectedvalue = ~(init);
+  }
+  else{
+    expectedvalue = ( out & 0x7FFF | ((out & 0x0600)^(out & 0x0100) ^ (out & 0x0008) ^ (out & 0x0002)));
+  }
+  
+  if(model.out != expectedvalue){
+  return 1;
+  }
 }
+
