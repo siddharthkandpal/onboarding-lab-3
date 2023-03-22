d63841d8ad8a609872cf1310179fa3dbceae34eb
Siddharth Kandpal <123426199+siddharthkandpal@users.noreply.github.com>
2023-03-22 15:26:18 -0400

    Updated ex1

--- dv/exercise1.cpp
+++ dv/exercise1.cpp
@@ -1,17 +1,44 @@
-#include <VExercise1.h>
-     
-int main() {
-  int h,i,k,a,b,op;
-  VExercise1 model;
-  for(h = 0; h < 3; h = h + 1){
-  for (i = 0; i < 256; i = i + 1){
-    for (k = 0; k < 256; k = k + 1){
-      
-    model.op = h;
-    model.a = i;
-    model.b = k;
-    model.eval();
-  }
- }
-  }
+#include <iostream>
+#include <cstdint>
+#include <stdlib.h>
+#include "VExercise1.h"
+
+int main(){
+
+VExercise1 model;
+bool verify = false;
+model.op = 0;
+model.a  = 0;
+model.b  = 0;
+				 
+for (model.op = 0; model.op < 4; model.op++) {
+	for (model.a = 0; model.a < 256; model.a++) {
+		for (model.b = 0; model.b < 256; model.b++) {
+			model.eval();
+			if ((model.op == 0) && ((model.a ^ model.b) != model.out)) {
+				verify = true;
+			}
+			
+			if ((model.op == 1) && ((model.a << model.b) != model.out)) {
+				verify = true;
+			}
+			
+			if ((model.op == 2) && ((model.b == 0) ? (0) : (model.a % model.b)) != model.out) {
+				verify = true;
+			}
+			
+			if ((model.op == 3) && (~(model.a & model.b) != model.out)) {
+				verify = true;
+			}
+		}
+	}
+}
+  
+if (verify) {
+	std::cout << "Failed!" << std::endl;
+	return 1;
+}
+
+std::cout << "Success!" << std::endl;
+return 0;
 }
