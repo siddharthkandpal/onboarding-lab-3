db1996e4f79d88c92a37d26dcd22da6ac8373a76
Siddharth Kandpal <123426199+siddharthkandpal@users.noreply.github.com>
2023-03-23 16:41:27 -0400

    Updated ex 2

--- dv/exercise2.cpp
+++ dv/exercise2.cpp
@@ -1,24 +1,49 @@
-#include <VExercise2.h>
-
-int main() {
-  uint8_t expectedvalue;
-  int reset, init, clk, out;
-  VExercise2 model;
-     for(reset = 0; reset < 2; reset++){
-    model.clk = 0;
-    model.init = 15;
-    model.eval();
-    }
-  
-  if(reset = 1){
-    expectedvalue = ~(init);
-  }
-  else{
-    expectedvalue = ( out & 0x7FFF | ((out & 0x0600)^(out & 0x0100) ^ (out & 0x0008) ^ (out & 0x0002)));
-  }
-  
-  if(model.out != expectedvalue){
-  return 1;
-  }
+#include <iostream>
+#include "VExercise2.h"
+#include <stdlib.h>
+#include <iostream>
+uint16_t lfsr(uint16_t lfsr_bits) {
+  //calculating the output of lfsr
+  return (lfsr_bits << 1) + (((lfsr_bits >> 10)  ^ (lfsr_bits >> 8)  ^ (lfsr_bits >> 3) ^ (lfsr_bits >> 1)) & 1);
 }
 
+//testing the module for reset = 1 and clock = 0
+
+bool input_test(uint16_t ) {
+uint16_t test_input;
+uint16_t temp;
+int j;
+
+VExercise2 model;
+model.clk = 1;
+model.eval();
+model.clk = 0;
+model.reset = 1;
+model.init = test_input;
+model.eval();
+
+
+// initializing
+
+temp = !test_input;
+
+for(j = 0; j < 500; j++){
+    model.clk = 0;
+    model.reset = 0;
+    model.eval();
+    model.clk = 1;
+    model.reset = 0;
+    model.eval();
+    if(model.out != lfsr(temp))
+    return true;
+    temp = model.out;}
+  return false;
+}
+
+int main() {
+ bool failed = false;
+ failed |= input_test(432);
+ failed |= input_test(23);
+ failed |= input_test(244);
+ return (int) failed;
+}
