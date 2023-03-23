15c038f6ccc172e3d79e703d328064fad76e6710
Siddharth Kandpal <123426199+siddharthkandpal@users.noreply.github.com>
2023-03-22 22:24:13 -0400

    Updated ex 4 passed

--- dv/exercise4.cpp
+++ dv/exercise4.cpp
@@ -1,36 +1,37 @@
+include <cstdint>
 #include <VExercise4.h>
-int main() {
-   uint16_t expectedvalue;
-  VExercise4 model; 
-  int cs, sel,alpha,beta,gamma;
-  for (cs = 0;cs < 2; cs = cs + 1){
-    for(sel = 0; sel < 4; sel = sel + 1){
-      model.alpha = 10;
-      model.beta = 20;
-      model.gamma = 30;
-      model.eval();
-    }
-  }
-  
-  if(cs = 0){
-      expectedvalue = 0;
-    }
-  while(expectedvalue != 0){
-    if(sel == 0 && cs == 1){
-      expectedvalue = 10;
-    }
-    else if(sel == 1 && cs ==1){
-      expectedvalue = 20;
-    }
-    else if(sel == 2 && cs == 1){
-      expectedvalue = 30;
-    }
-    else{
-      expectedvalue = (10 | ( 20 & 30 ));
-    }
-  }
-    
-  if (model.out != expectedvalue){
-    return 1;
- }
+#include <stdlib.h>
+#include <iostream>
+
+int main(){
+VExercise4 model;
+int k;
+model.alpha = 1;
+model.beta = 2;
+model.gamma = 3;
+bool verify = true;
+model.cs = 0;
+for(model.sel = 0; model.sel < 4; model.sel++){
+model.eval();
+if(model.out != 0)
+{
+verify = false;
+}
+}
+
+model.cs = 1;
+for(k = 0; k < 4; k++){
+model.eval();
+switch(k) {
+  case 0: verify = verify && (model.out == 1);
+  break;
+  case 1: verify = verify && (model.out == 2);
+  break;
+  case 2: verify = verify && (model.out == 3);
+  break;
+  case 3: verify = verify && (model.out == 1 & (2 | 3));
+  break;
+}
+return 0;
+}
 }
